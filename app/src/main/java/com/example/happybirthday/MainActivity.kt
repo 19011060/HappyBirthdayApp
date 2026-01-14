7358db07d0a5a3f3c329bce58189d165bc9577e1
19011060 <cade.blanco@go.sfcollege.edu>
2026-01-14 00:19:39 -0500

    Removed and added some functions. Added comments for clarity and made new imports to support the use of Surface() and MaterialTheme.

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,29 +21,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    // This function displays text in the UI by calling the Text() composable function
+    @Composable
+    fun GreetingText(message: String, modifier: Modifier = Modifier) {
+        Text(text = message)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        Greeting("Cade")
+    // This function calls the "GreetingText" function and displays it in the UI preview
+    @Preview(showBackground = true)
+    @Composable
+    fun BirthdayCardPreview() {
+        HappyBirthdayTheme {
+            GreetingText(message = "Happy Birthday Cade!")
+        }
     }
 }
