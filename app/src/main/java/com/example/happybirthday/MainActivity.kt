4787b8d591dc3debac6e4b73f9df272d28a86883
19011060 <cade.blanco@go.sfcollege.edu>
2026-01-14 22:07:49 -0500

    Added extra text and some formatting. Added extra comments for clarity.

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -5,14 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +32,16 @@ class MainActivity : ComponentActivity() {
 
     // This function displays text in the UI by calling the Text() composable function
     @Composable
-    fun GreetingText(message: String, modifier: Modifier = Modifier) {
-        Text(text = message)
+    fun GreetingText(message: String, from : String, modifier: Modifier = Modifier) {
+        Text( // Main message
+            text = message,
+            fontSize = 100.sp, // Changes the font size
+            lineHeight = 116.sp
+        )
+        Text( // Second message
+            text = from,
+            fontSize = 36.sp // Changes the line height
+        )
     }
 
     // This function calls the "GreetingText" function and displays it in the UI preview
@@ -42,7 +49,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun BirthdayCardPreview() {
         HappyBirthdayTheme {
-            GreetingText(message = "Happy Birthday Cade!")
+            GreetingText(message = "Happy Birthday Madie!", from = "From Cade") // Function call, text arguments passes
         }
     }
 }
