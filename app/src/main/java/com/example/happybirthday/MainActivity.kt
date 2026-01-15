d25b0d5d53d3cc40c0165ca22d416f5cacd219ab
19011060 <cade.blanco@go.sfcollege.edu>
2026-01-14 23:48:17 -0500

    Final Commit & Push. Replaced the hardcoded string values in OnCreate() with string resources and getString() function calls to the strings.xml file.

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,8 +37,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Madie!",
-                        from = "From Cade"
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
                     )
                 }
             }
@@ -62,7 +63,7 @@ class MainActivity : ComponentActivity() {
                 fontSize = 36.sp, // Changes the font size
                 modifier = Modifier
                     .padding(16.dp)
-                    .align(alignment = Alignment.End)
+                    .align(alignment = Alignment.CenterHorizontally)
             )
         }
     }
